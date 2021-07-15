09d32b087f53c655b193dd789a0190413f1f157d
davidyeghiazaryan <david.yeghiazaryan@coinstats.app>
2021-07-15 11:00:51 +0400

    xoski

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
- import React from 'react';
+import React from 'react';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import {HashRouter as Router, Switch, Route, Link} from 'react-router-dom'
 
 import News from './pages/NewsPage';
 
